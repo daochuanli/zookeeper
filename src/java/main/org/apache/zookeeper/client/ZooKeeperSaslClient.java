a34e647ad0d15ff258b063bdc944d4ad210fe121
Eugene Koontz <ekoontz@hiro-tan.org>
2011-07-01 16:43:33 -0700

    working on access to client-related networking stuff as needed.

--- src/java/main/org/apache/zookeeper/client/ZooKeeperSaslClient.java
+++ src/java/main/org/apache/zookeeper/client/ZooKeeperSaslClient.java
@@ -18,12 +18,24 @@
 
 package org.apache.zookeeper.client;
 
+import javax.security.auth.Subject;
 import javax.security.sasl.SaslClient;
+import javax.security.sasl.SaslException;
 
+import org.apache.zookeeper.AsyncCallback;
+import org.apache.zookeeper.ClientCnxn;
 import org.apache.zookeeper.LoginThread;
+import org.apache.zookeeper.ZooDefs;
+import org.apache.zookeeper.data.Stat;
+import org.apache.zookeeper.proto.GetSASLRequest;
+import org.apache.zookeeper.proto.ReplyHeader;
+import org.apache.zookeeper.proto.RequestHeader;
+import org.apache.zookeeper.proto.SetSASLResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.apache.zookeeper.ClientCnxn;
+
+import java.security.PrivilegedActionException;
+import java.security.PrivilegedExceptionAction;
 
 /**
  * This class manages SASL authentication and, optionally, encryption for the client. It
@@ -35,6 +47,114 @@ public class ZooKeeperSaslClient {
     private LoginThread loginThread;
     private SaslClient saslClient;
     private byte[] saslToken = new byte[0];
+    private ClientCnxn cnxn;
 
+    public ZooKeeperSaslClient(ClientCnxn cnxn) {
+      this.cnxn = cnxn;
+    }
+
+    public void prepareSaslResponseToServer(byte[] serverToken) {
+        saslToken = serverToken;
+
+        LOG.debug("saslToken (server) length: " + saslToken.length);
+
+        if (!(saslClient.isComplete() == true)) {
+            try {
+                saslToken = createSaslToken(saslToken, saslClient);
+                if (saslToken != null) {
+                    LOG.debug("saslToken (client) length: " + saslToken.length);
+                    queueSaslPacket(saslToken);
+                }
+
+                if (saslClient.isComplete() == true) {
+                    LOG.info("SASL authentication with Zookeeper server is successful.");
+                    // FIXME: cnxn.eventThread is not visible from here.
+/*                    cnxn.eventThread.queueEvent(new WatchedEvent(
+                      Watcher.Event.EventType.None,
+                      Watcher.Event.KeeperState.SaslAuthenticated, null));*/
+                }
+
+            } catch (SaslException e) {
+                // TODO sendThread should set state to AUTH_FAILED; but currently only sendThread modifies state.
+                LOG.error("SASL authentication failed.");
+            }
+        }
+    }
+
+    byte[] createSaslToken(final byte[] saslToken, final SaslClient saslClient) throws SaslException {
+        if (saslToken == null) {
+            // TODO: introspect about runtime environment (such as jaas.conf)
+            throw new SaslException("Error in authenticating with a Zookeeper Quorum member: the quorum member's saslToken is null.");
+        }
+
+        Subject subject = this.loginThread.getLogin().getSubject();
+        if (subject != null) {
+            synchronized(this.loginThread) {
+                try {
+                    final byte[] retval =
+                        Subject.doAs(subject, new PrivilegedExceptionAction<byte[]>() {
+                                public byte[] run() throws SaslException {
+                                    try {
+                                        LOG.debug("ClientCnxn:createSaslToken(): ->saslClient.evaluateChallenge(len="+saslToken.length+")");
+                                        return saslClient.evaluateChallenge(saslToken);
+                                    }
+                                    catch (NullPointerException e) {
+                                        LOG.error("Quorum Member's SASL challenge was null.");
+                                    }
+                                    // NOTE: saslClient.evaluateChallenge() will throw a SaslException if authentication fails.
+                                    // returning null here will cause another (new) SaslException to be thrown.
+                                    return null;
+                                }
+                            });
+
+                    if (retval != null) {
+                        LOG.debug("Successfully created token with length:"+retval.length);
+                    }
+
+                    return retval;
+                }
+                catch (PrivilegedActionException e) {
+                    LOG.error("An error: " + e + " occurred when evaluating Zookeeper Quorum Member's received SASL token. Client will go to AUTH_FAILED state.");
+                    throw new SaslException("An error: " + e + " occurred when evaluating Zookeeper Quorum Member's received SASL token. Client will go to AUTH_FAILED state.");
+                }
+            }
+        }
+        else {
+            throw new SaslException("Cannot make SASL TOKEN without subject defined.");
+        }
+    }
+
+     static class ServerSaslResponseCallback implements AsyncCallback.DataCallback {
+        public void processResult(int rc, String path, Object ctx, byte data[], Stat stat) {
+            // data[] contains the Zookeeper Server's SASL token.
+            // ctx is the ClientCnxn object. We use this object's prepareSaslResponseToServer() method
+            // to reply to the Zookeeper Server's SASL token
+            ClientCnxn cnxn = (ClientCnxn)ctx;
+            byte[] usedata = data;
+            if (data != null) {
+                LOG.debug("ServerSaslResponseCallback(): saslToken server response: (length="+usedata.length+")");
+            }
+            else {
+                usedata = new byte[0];
+                LOG.debug("ServerSaslResponseCallback(): using empty data[] as server response (length="+usedata.length+")");
+            }
+            cnxn.prepareSaslResponseToServer(usedata);
+        }
+    }
+
+    private void queueSaslPacket(byte[] saslToken) {
+        LOG.debug("ClientCnxn:sendSaslPacket:length="+saslToken.length);
+        RequestHeader h = new RequestHeader();
+        h.setType(ZooDefs.OpCode.sasl);
+        GetSASLRequest request = new GetSASLRequest();
+        request.setToken(saslToken);
+        SetSASLResponse response = new SetSASLResponse();
+
+        ServerSaslResponseCallback cb = new ServerSaslResponseCallback();
+
+        ReplyHeader r = new ReplyHeader();
+// FIXME: cnxn.queuePacket is not visible from here:
+//        cnxn.queuePacket(h, r, request, response, cb, null, null, this, null);
+    }
 
 }
