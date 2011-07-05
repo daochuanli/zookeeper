c71a1aef117bd0165217ec63a93e6c645e23416a
Eugene Koontz <ekoontz@hiro-tan.org>
2011-07-05 11:03:00 -0700

    remove unused import

--- src/java/main/org/apache/zookeeper/server/quorum/QuorumPeerMain.java
+++ src/java/main/org/apache/zookeeper/server/quorum/QuorumPeerMain.java
@@ -21,7 +21,6 @@ import java.io.File;
 import java.io.IOException;
 
 import javax.management.JMException;
-import javax.security.auth.Subject;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
