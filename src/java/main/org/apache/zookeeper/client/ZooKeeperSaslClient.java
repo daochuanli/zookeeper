9a8a2f91585164b23ddd778fa9cf49c5b07b5f99
Eugene Koontz <ekoontz@hiro-tan.org>
2012-06-03 11:17:30 +0200

    minimizing trunk diffs

--- src/java/main/org/apache/zookeeper/client/ZooKeeperSaslClient.java
+++ src/java/main/org/apache/zookeeper/client/ZooKeeperSaslClient.java
@@ -18,7 +18,11 @@
 
 package org.apache.zookeeper.client;
 
-import org.apache.zookeeper.*;
+import org.apache.zookeeper.AsyncCallback;
+import org.apache.zookeeper.ClientCnxn;
+import org.apache.zookeeper.ClientCnxnSocket;
+import org.apache.zookeeper.Login;
+import org.apache.zookeeper.ZooDefs;
 import org.apache.zookeeper.data.Stat;
 import org.apache.zookeeper.proto.GetSASLRequest;
 import org.apache.zookeeper.proto.ReplyHeader;
